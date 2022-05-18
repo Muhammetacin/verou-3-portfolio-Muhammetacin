3bd4661daf0f5b0e10bd95977bc91f3951411274
Muhammet Cin <muhammetacin@gmail.com>
2022-05-18 15:46:34 +0200

    Write text about me

--- src/About.js
+++ src/About.js
@@ -17,15 +17,15 @@ const About = () => {
           "url(https://images.pexels.com/photos/2929300/pexels-photo-2929300.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)",
       }}
     >
-      <div className="hero-overlay bg-opacity-50 bg-black"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-lg">
+      <div className="hero-overlay bg-opacity-60 bg-black"></div>
+      <div className="hero-content text-neutral-content">
+        <div className="max-w-2xl text-lg">
           <Center>
             <Wrap>
               <WrapItem>
-                <Avatar size="xl" name='Muhammet Cin'>
+                <Avatar size="2xl" name="Muhammet Cin">
                   <AvatarBadge
-                    boxSize="1.25em"
+                    boxSize="0.9em"
                     bg="green.500"
                     borderColor="green.400"
                   />
@@ -33,7 +33,35 @@ const About = () => {
               </WrapItem>
             </Wrap>
           </Center>
-          <h2 className="my-3 text-xl font-semibold">Learn more about me.</h2>
+          <h2 className="my-3 text-3xl font-semibold">Learn more about me.</h2>
+          <div className="font-semibold text-xl">
+            <br />
+            <p>
+              <i>
+                "Everything is fun unless you have no knowledge of it."
+                <br />
+                &#8212; Muhammet Cin
+              </i>
+            </p>
+            <br />
+            <p>
+              When I was a young boy who played a lot of video games I sometimes
+              got frustrated with the limitations. With that frustration I
+              started to research how I could give myself the advantage in
+              games. Because winning is always more fun than losing &#128516;
+              <br />
+              <br />I started to tweak the source files of the games I played.
+              For example, Age of Empires 3. Briefly, in the game there are
+              factions and unique buildings for each with different benefits.
+              The Netherlands has Bank which generates money. The Ottomans have
+              Factory building that can create The Great Bombard (most powerful
+              cannon in game). But I wanted them both and have no limits on
+              either so I went into the code and played around the get them all
+              available. Since then I am excited to create and solve problems.
+              Solving problems gives me satisfaction so I believe I have found
+              the perfect job for the rest of my life: coding!
+            </p>
+          </div>
         </div>
       </div>
     </div>
